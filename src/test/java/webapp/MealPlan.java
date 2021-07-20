ef875fc55ec007affc056c0b6333e0432793ed67
m.kozak <kozak.mariia1@gmail.com>
2021-07-20 11:20:44 +0300

    fixed selectors

--- src/test/java/webapp/MealPlan.java
+++ src/test/java/webapp/MealPlan.java
@@ -1,12 +1,7 @@
 package webapp;
 
 import com.codeborne.selenide.*;
-import com.codeborne.selenide.impl.ElementsContainerCollection;
-import org.testng.Assert;
-import org.testng.AssertJUnit;
 import org.testng.annotations.Test;
-
-import javax.lang.model.element.Element;
 
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.$$;
@@ -16,13 +11,14 @@ public class MealPlan {
 
     @Test
     public void verifyUserCanLoginWithValidParameters() {
-        // configure browser
+
+
         Configuration.browser = Browsers.CHROME;
         Selenide.open("https://app.dev.betterme.world/login");
-        SelenideElement emailInput =  $("input[data-input='email-login-email']");
-        SelenideElement passwordInput =  $("input[data-input='email-login-password']");
-        SelenideElement loginButton =  $("div[class*='login_button'] button");
-        SelenideElement dinner = $("[href='/meal?selectedDate=2021-07-19&currentDish=431847']");
+        SelenideElement emailInput = $("input[data-input='email-login-email']");
+        SelenideElement passwordInput = $("input[data-input='email-login-password']");
+        SelenideElement loginButton = $("div[class*='login_button'] button");
+        SelenideElement dinner = $("div.container_container__3P--6.container_lg__3hN4P > div > section > div:nth-child(4)");
         SelenideElement image = $("div[class*='meal_imageTablet__4zk0z']");
 
         emailInput.setValue("gifatog434@godpeed.com");
@@ -30,14 +26,12 @@ public class MealPlan {
         loginButton.click();
         dinner.click(); //переписати селектор щоб був на будь-який день
 
-        ElementsCollection description = $$("div[class = 'meal_howToMakeTablet__14h9b']");
-        ElementsCollection ingredients = $$("ul[class='ingredients_list__2rQQc']");
-        image.isDisplayed();
+        ElementsCollection description = $$("div:nth-child(4) > div > div.meal_columnLeft__OGThl > div.meal_howToMakeTablet__14h9b > ul");
+        ElementsCollection ingredient = $$("div.meal_columnRight__1tEC7 > div.meal_ingredients__1Uq1K > ul");
+
+        image.exists();
         assertThat(description).isNotNull(); //не факт що норм перевірка
-        assertThat(ingredients).isNotNull();
-
-
-
+        assertThat(ingredient).isNotNull();
 
     }
 }
